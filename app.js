98e738ac97d5d5330e121126270fee4bd6273baf
GOSEUNGHYUN <156834770+GOSEUNGHYUN@users.noreply.github.com>
2024-05-02 00:32:52 +0900

    2.4 Booleans

--- app.js
+++ app.js
@@ -1,20 +1,5 @@
-const a = 5;
-const b = 2;
-//const(constant:상수), 값이 바뀔 수 없다.
-//규칙을 알면, 코드가 나중에 뭘 할지 알 수 있다.
-let myName = "nico";
-//바뀔 수도 있는 variable
-
-console.log(a+b);
-console.log(a*b);
-console.log(a/b);
-console.log("hello " + myName);
-
-myName = "nicolas";
-
-console.log("your new name is " + myName);
-
-//많은 사람들이 const를 기본적으로 사용하고, 만약 variable을 업데이트 하고 싶다면 let을 쓴다.
-//오래된 자바스크립트 강좌나 책에서 var을 볼 수 있다. var은 어떠한 규칙도 가지고 있지 않다.
-
-//규칙 : 기본적으로 const를 쓰고 필요할 때만 let을 쓰되, var은 쓰지 말 것.
+const amIFat = null; //null 값, true와 false
+//null : 비어있음
+let something; //undefined 타입
+//undefined : variable이 메모리에 만들어졌지만 컴퓨터가 이 variable에 대해서 인지하고 있지만, 값이 없는 것이다.
+console.log(something, amIFat);
