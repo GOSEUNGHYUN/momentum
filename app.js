feb5b70f273ffb7ba173c30a96fbc6c9084780da
GOSEUNGHYUN <156834770+GOSEUNGHYUN@users.noreply.github.com>
2024-05-02 00:39:27 +0900

    2.5 Arrays

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-const amIFat = null; //null 값, true와 false
-//null : 비어있음
-let something; //undefined 타입
-//undefined : variable이 메모리에 만들어졌지만 컴퓨터가 이 variable에 대해서 인지하고 있지만, 값이 없는 것이다.
-console.log(something, amIFat);
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+console.log(daysOfWeek);
+
+//array에서 아이템 받아오기
+console.log(daysOfWeek[4]);
+
+//array안에 요일 더 추가하기
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
