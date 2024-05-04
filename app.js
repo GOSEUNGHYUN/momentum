24c2917e18b35ead37d0be156e2b3b520442f2f0
GOSEUNGHYUN <156834770+GOSEUNGHYUN@users.noreply.github.com>
2024-05-04 21:24:47 +0900

    2.10 추가설명
    object, function

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-function sayHello(nameOfPerson, age){
-    console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
+//추가설명
+
+//object
+const person = {
+    name:"Nico",
+    age: 98,
+};
+console.log(person);
+
+person.sexy = "soon"; //object안에 새 property를 만들 수 있다.
+console.log(person);
+
+
+//function
+function plus(potato, salad){ //potato, salad는 data를 받으면 대체되는 place holder(자리표시자)일 뿐이다.
+    console.log(potato+salad);
 }
 
-sayHello("nico", 10);
-sayHello("dal", 23);
-sayHello("lynn", 21);
-
-
-const player = {
-    name: "nico",
-    sayHello: function(otherPersonsName){
-        console.log("hello " + otherPersonsName + " nice to meet you!");
-    },
-}
-
-console.log(player.name);
-player.sayHello("lynn");
+plus(5,10);
