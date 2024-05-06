4c95c9ca28853d0b067beba1947a22449a439d6d
GOSEUNGHYUN <156834770+GOSEUNGHYUN@users.noreply.github.com>
2024-05-06 14:13:56 +0900

    2.14 Conditionals part Two

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
+const age = parseInt(prompt("How old are you?"));
 
-const age = prompt("How old are you?");
-//prompt(message,default) : 사용자에게 창을 띄울 수 있게 해주고 유저에게 값을 물어볼 수 있다.
-//argument = (string)message, default
-//javascript의 코드 실행을 멈추게한다. 답을 들을 때 가지 기다린다.
-//-> prompt function을 사용하지 않는 이유, 요즘은 사용하지않는다.
+console.log(isNaN(age));
+//isNaN ? (NaN)true : (NotNaN)false
 
-parseInt(age);
-//string -> number
-
-console.log(typeof age,typeof parseInt(age));
-//타입 확인하기 : typeof variable
-
-console.log(age, parseInt(age));
-
-
-//age에 숫자가 입력되었다면 number, 문자가 입력되었다면 NaN(Not a Number).
-const agee = parseInt(prompt("How old are you?"));
-
-console.log(agee);
+if(isNaN(age)){
+    console.log("Please write a number");
+}else{
+    console.log("Thank you for writing your age.");
+}
