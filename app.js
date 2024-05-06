578d3f731311146216342a792c5b5d83549995bd
GOSEUNGHYUN <156834770+GOSEUNGHYUN@users.noreply.github.com>
2024-05-06 12:51:35 +0900

    2.14 Conditionals part One

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
-const calculator = {
-    add : function(a,b){
-        console.log(a+b);
-    },
-    sub : function(a,b){
-        console.log(a-b);
-    },
-    div : function(a,b){
-        console.log(a/b);
-    },
-    mul : function(a,b){
-        console.log(a*b);
-    },
-    powerof : function(a){
-        console.log(a*a);
-    }
-}
 
-calculator.add(1,1);
-calculator.div(6,2);
-calculator.mul(2,2);
-calculator.powerof(10);
+const age = prompt("How old are you?");
+//prompt(message,default) : 사용자에게 창을 띄울 수 있게 해주고 유저에게 값을 물어볼 수 있다.
+//argument = (string)message, default
+//javascript의 코드 실행을 멈추게한다. 답을 들을 때 가지 기다린다.
+//-> prompt function을 사용하지 않는 이유, 요즘은 사용하지않는다.
+
+parseInt(age);
+//string -> number
+
+console.log(typeof age,typeof parseInt(age));
+//타입 확인하기 : typeof variable
+
+console.log(age, parseInt(age));
+
+
+//age에 숫자가 입력되었다면 number, 문자가 입력되었다면 NaN(Not a Number).
+const agee = parseInt(prompt("How old are you?"));
+
+console.log(agee);
