4926f8c4a995401e9ab4b108d2b3aef8616c81a6
GOSEUNGHYUN <156834770+GOSEUNGHYUN@users.noreply.github.com>
2024-05-16 22:20:28 +0900

    #4.2 Events

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 const loginForm = document.querySelector('#login-form');
-const loginInput = loginForm.querySelector('input');
-const loginButton = loginForm.querySelector('button');
-// --> const loginInput = document.querySelector("#login-form input");
-// --> const loginButton = document.querySelector("#login-form button");
-// --> 두 줄로도 가능
+const loginInput = document.querySelector('#login-form input');
 
-function onLoginBtnClick() {
-    const username = loginInput.value;
-    if (username === '') {
-        alert('Please write your name');
-    } else if (username.length > 15) {
-        alert('Your name is too long');
-    }
+function onLoginSubmit(event) {
+    //js에서 첫번째argument로, 발생된 event에 대한 정보를 채워준다. 공백은 어떠한 정보도 받지 않겠다는 것이다.
+
+    //submit은 엔터를 누르거나 버튼을 클릭할 때 발생한다.
+    //브라우저는 엔터를 누를 때 새로고침을 하고 form을 submit하도록 되어있다.
+    event.preventDefault(); //어떤 event의(브라우저의) 기본행동이든지 발생되지 않도록 막는 function이다.
+    console.log(event); //tomato(event)에는 방금 실행된 event 정보가 담겨있다.
+    console.log(loginInput.value);
 }
 
-loginButton.addEventListener('click', onLoginBtnClick);
+loginForm.addEventListener('submit', onLoginSubmit);
