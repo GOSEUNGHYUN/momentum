c0f170474ff1a271c00549a0442774a9efaa877b
GOSEUNGHYUN <156834770+GOSEUNGHYUN@users.noreply.github.com>
2024-05-17 10:30:34 +0900

    #4.3 Events part Two

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
 
-function onLoginSubmit(event) {
-    //js에서 첫번째argument로, 발생된 event에 대한 정보를 채워준다. 공백은 어떠한 정보도 받지 않겠다는 것이다.
+const link = document.querySelector('a');
 
-    //submit은 엔터를 누르거나 버튼을 클릭할 때 발생한다.
-    //브라우저는 엔터를 누를 때 새로고침을 하고 form을 submit하도록 되어있다.
-    event.preventDefault(); //어떤 event의(브라우저의) 기본행동이든지 발생되지 않도록 막는 function이다.
-    console.log(event); //tomato(event)에는 방금 실행된 event 정보가 담겨있다.
+function onLoginSubmit(event) {
+    event.preventDefault();
+    console.log(event);
     console.log(loginInput.value);
 }
 
+function handleLinkClick(event) {
+    event.preventDefault();
+    console.dir(event);
+}
+
 loginForm.addEventListener('submit', onLoginSubmit);
+link.addEventListener('click', handleLinkClick);
+//가끔은 기본 동작을 막아야할 필요가 있고, 가끔은 뭐가 클릭됐는지 어디가 클린됐는지 알고 싶을 때가 있다.
+//JS는 함수를 실행시키는 동시에 그 함수에 첫번째 인자로 object를 넣어준다.
+//object에는 방금 일어난 event에 대한 여러 정보가 담겨있다.
