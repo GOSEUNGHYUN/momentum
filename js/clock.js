fc4f75cd004b2be210a6202b855a8c9d9d363940
GOSEUNGHYUN <156834770+GOSEUNGHYUN@users.noreply.github.com>
2024-05-18 22:27:06 +0900

    #5.1 Timeouts and Dates

--- js/clock.js
+++ js/clock.js
@@ -1,9 +1,8 @@
 const clock = document.querySelector('h2#clock');
 
-function sayHello() {
-    console.log('Hello');
+function getClock() {
+    const date = new Date();
+    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
-
-//interval : '매번' 일어나야 하는 무언가를 의미한다. ex) 매2초
-//setInterval(실행하고자하는 function, function의 간격을 몇 ms(milliseconds)로 할 지);
-setInterval(sayHello, 5000);
+getClock();
+setInterval(getClock, 1000);
